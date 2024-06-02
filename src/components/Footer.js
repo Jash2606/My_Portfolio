959fecdb9904cee3c467b6bdb6b28739e172b3d4
Jash Savaliya <jash.23bcs10163@sst.scaler.com>
2024-06-02 22:39:13 +0530

    Added Link in buttons of footer..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ import { FaFacebook, FaGithub, FaHome, FaLinkedin, FaMailBulk , FaPhone} from 'r
         <div className="footer-container">
             <div className="left">
                 <div className="location">
-                   <FaHome size={20} style={{color:"#fff", marginRight:"2rem"}}/>
+                   <FaHome size={20} style={{color:"#fff", marginRight:"2rem"}} />
                    <div>
                         <p> 9/A Panchvati Soc.</p>
                         <p> Surat,Gujarat. </p>
@@ -28,8 +28,12 @@ import { FaFacebook, FaGithub, FaHome, FaLinkedin, FaMailBulk , FaPhone} from 'r
                 <p> This is me Jash Savaliya , Curious Learner and Explorer First Year UG Student at Scaler School Of Technology. </p>
                 <div className="social">
                     <FaFacebook size={20} style={{color:"#fff", marginRight:"1rem"}}/>
-                    <FaGithub size={20} style={{color:"#fff", marginRight:"1rem"}}/>
-                    <FaLinkedin size={20} style={{color:"#fff", marginRight:"1rem"}}/>
+                    < a href="https://github.com/Jash2606">
+                        <FaGithub size={20} style={{color:"#fff", marginRight:"1rem"}} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/jash-savaliya-7b0b3b1b3/">
+                        <FaLinkedin size={20} style={{color:"#fff", marginRight:"1rem"}}/>
+                    </a>
                 </div>
             </div>
         </div>
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -17,7 +17,7 @@ const Project = () => {
           text= "Some of My Recent Learning"
         /> */}
         <Cardtype 
-          header='Hey Google'
+          header='Project Title'
           subheader='This is a awesome card'
           img='https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           text='This is text card will use to wrote project description and other stuff.'
